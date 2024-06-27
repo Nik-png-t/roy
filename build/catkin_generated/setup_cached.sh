37a87b6c0f2818c727b522e597985cc5f14019bd
Voropanov Nikita <nikvoropanov74@gmail.com>
2024-06-27 22:02:24 +0700

    add multiple connect and takeoff

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,7 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/nikita/catkin_ws/src/Roy/build/devel:$CMAKE_PREFIX_PATH"
-export PATH='/opt/ros/noetic/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/nikita/.local/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/nikita/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PATH='/opt/ros/noetic/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/nikita/.local/bin:/usr/lib/ccache:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/nikita/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/nikita/ardupilot/Tools/autotest:/home/nikita/.local/bin'
 export PWD='/home/nikita/catkin_ws/src/Roy/build'
-export ROSLISP_PACKAGE_DIRECTORIES="/home/nikita/catkin_ws/src/Roy/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH='/home/nikita/catkin_ws/src/Roy:/home/nikita/catkin_ws/src/.:/opt/ros/noetic/share'
