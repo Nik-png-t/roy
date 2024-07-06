9e04ce9e9bb7d4b3e558d5f354abe2077d1be949
Voropanov Nikita <nikvoropanov74@gmail.com>
2024-07-06 17:10:32 +0700

    start leader branch

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,5 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/nikita/catkin_ws/src/Roy/build/devel:$CMAKE_PREFIX_PATH"
-export PATH='/opt/ros/noetic/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/nikita/.local/bin:/usr/lib/ccache:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/nikita/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/nikita/ardupilot/Tools/autotest:/home/nikita/.local/bin'
-export PWD='/home/nikita/catkin_ws/src/Roy/build'
+export PATH='/opt/ros/noetic/bin:/usr/lib/ccache:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/nikita/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/nikita/ardupilot/Tools/autotest:/home/nikita/.local/bin'
 export ROS_PACKAGE_PATH='/home/nikita/catkin_ws/src/Roy:/home/nikita/catkin_ws/src/.:/opt/ros/noetic/share'
