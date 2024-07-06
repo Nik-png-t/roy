5ed64e05b305fa04ed7bba03f5b70aa6c1314996
Voropanov Nikita <nikvoropanov74@gmail.com>
2024-07-06 17:23:45 +0700

    deleting automoc

---
+++ src/led_controller_node.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <led_controller.hpp>
+
+
+int main(int argc, char *argv[]){
+	if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    ros::init(argc, argv, "led_controller_node");
+	ros::NodeHandle n;
+	ros::Rate rate(60);
+	int number_of_drone = 1;
+    ros::Time last_request = ros::Time::now();
+    LedController led = LedController(n, to_string(number_of_drone), argv[1], argv[2]);
+
+	while(ros::ok())
+	{  
+        if(led.local_armed() != led.leader_armed()){
+            led.arm(led.leader_armed());
+            last_request = ros::Time::now();
+        }
+        led.update();
+		ros::spinOnce();
+        rate.sleep();
+	}
+    return 0;
+}
